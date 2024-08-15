a2fe973fae8b5209498b20caccca6b14eaa42804
Alvaro Isea <alvaro.isea@pulselive.com>
2024-08-15 15:04:29 +0200

    clean up

--- notelist/build.gradle.kts
+++ notelist/build.gradle.kts
@@ -8,16 +8,11 @@ plugins {
 
 apply(from = "$rootDir/feature-dependencies.gradle")
 apply(from = "$rootDir/android-config.gradle")
+apply(from = "$rootDir/test-dependencies.gradle")
 
 android.namespace = "com.ai.notelist"
 
 dependencies {
     implementation(project(":common"))
     implementation(project(":common-android-data"))
-
-    testImplementation(libs.mockk)
-    testImplementation(libs.mockk.android)
-    testImplementation(libs.mockk.agent)
-    testImplementation(libs.testng)
-    testImplementation(libs.coroutines.test)
 }
